8d2768bee7537ae051a28e12dfc726ce97e96646
kuluoluohaoxiuyi <kuluoluohaoxiuyi@gmail.com>
2020-05-19 23:52:10 +0800

    修改页面样式

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -16,9 +16,6 @@
               <el-tab-pane label="Timeline" name="timeline">
                 <timeline />
               </el-tab-pane>
-              <el-tab-pane label="Account" name="account">
-                <account :user="user" />
-              </el-tab-pane>
             </el-tabs>
           </el-card>
         </el-col>
@@ -32,11 +29,10 @@
 import UserCard from './components/UserCard'
 import Activity from './components/Activity'
 import Timeline from './components/Timeline'
-import Account from './components/Account'
 
 export default {
   name: 'Profile',
-  components: { UserCard, Activity, Timeline, Account },
+  components: { UserCard, Activity, Timeline },
   data () {
     return {
       user: {
